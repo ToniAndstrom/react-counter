11b1469d27bed3edb89b56dc0da48e0570f38f13
Toni Andström <toni_andstrom@hotmail.com>
2024-04-06 16:39:14 +0300

    changes

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,15 @@
 
 import {FC} from "react";
+import logo from "./assets/react.svg"
 
 interface Props {}
 
 const Header: FC<Props> = () => {
   return (
-    <div>Header</div>
+    <div className="py-3 px-2 " style={{borderBottom: "3px solid white"}}>
+      <img src={logo}alt="logo" style={{height:"100px", verticalAlign:"top"}} />
+      <span className="h2, pt -4 m-2 text-white">REACT-COUNTER</span>
+    </div>
   )
 };
 
